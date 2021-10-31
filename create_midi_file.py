5298f4ce10179e609ba23a1dda5aac7ae3693d8a
Fersyvanderzee <62746506+Fersyvanderzee@users.noreply.github.com>
2021-10-31 16:57:51 +0100

    Changed output message when done. It showed all the pitches, which you don't really need. Now it shows that it is done processing and shows the filename you chose.

--- create_midi_file.py
+++ create_midi_file.py
@@ -4,30 +4,29 @@ from scales import *
 
 def create_midi_file(bpm, duration_input, scale, length, track_name):
     mf = MIDIFile(1)
-    pitches = []
     time = 0
     track = 0
-
 
     mf.addTrackName(track, time, "Test1")
     mf.addTempo(track, time, bpm)
 
     for i in range(length):
-        pitch = allowed_pitches(scale)[random.randint(0, 6)]
-        if duration_input == 'random':
-            duration = random.randint(1, 4)
-        else:
-            duration = 1
+        if time <= length:
+            pitch = allowed_pitches(scale)[random.randint(0, 6)]
+            if duration_input == 'random':
+                duration = random.randint(1, 4)
+            else:
+                duration = 1
 
-        mf.addNote(track, 0, pitch, time, duration, 100)
-        time += duration
-        pitches.append(pitch)
-        print(str(pitch) + ' : ' + str(duration))
+            mf.addNote(track, 0, pitch, time, duration, 100)
+            time += duration
 
     with open('output/' + track_name + '.mid', 'wb') as output_file:
         mf.writeFile(output_file)
-    print(pitches)
+
+    print(f'Created midi file: {track_name}.mid')
 
 
-create_midi_file(bpm=120, duration_input='random', scale='Cmaj', length=40, track_name='test1')
-create_midi_file(bpm=120, duration_input='random', scale='Cmaj', length=40, track_name='test2')
+create_midi_file(bpm=174, duration_input='random', scale='Amaj', length=80, track_name='test1')
+create_midi_file(bpm=174, duration_input='random', scale='Amaj', length=80, track_name='test2')
+create_midi_file(bpm=174, duration_input='random', scale='Amaj', length=80, track_name='test3')
